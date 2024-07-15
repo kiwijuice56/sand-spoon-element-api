995f286fba0466bd655d83d54379a38225c01120
kiwijuice56 <alfaroeric127@gmail.com>
2024-07-15 02:08:34 -0500

    Added token tracking

--- app.py
+++ app.py
@@ -18,34 +18,59 @@ def get_custom_element():
         return {}
 
     response = {}
+    total_tokens = 0
 
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": "Answer in hex format only"},
+            {"role": "system", "content": "Answer in hex format only (ex: #FF0A19)"},
             {"role": "user", "content": f"Main color of: {element_name}"},
         ]
     )
     response["color_0"] = completion.choices[0].message.content
+    total_tokens += completion["usage"].total_tokens
 
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": "Answer in hex format only"},
+            {"role": "system", "content": "Answer in hex format only (ex: #FF0A19)"},
             {"role": "user", "content": f"Secondary color of: {element_name}"},
         ]
     )
     response["color_1"] = completion.choices[0].message.content
+    total_tokens += completion["usage"].total_tokens
 
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": "Answer in 1 word"},
+            {"role": "system", "content": "Answer using only an exact item from the list"},
             {"role": "user", "content": f"From this list of choices: [liquid, gas, rigid_solid, powdery_solid, life], pick the best description of {element_name}"}
         ]
     )
     response["state"] = completion.choices[0].message.content
+    total_tokens += completion["usage"].total_tokens
 
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "Answer in one character only: Y or N"},
+            {"role": "user", "content": f"Is {element_name} flammable?"}
+        ]
+    )
+    response["flammable"] = completion.choices[0].message.content
+    total_tokens += completion["usage"].total_tokens
+
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "Answer in one character only: Y or N"},
+            {"role": "user", "content": f"Is {element_name} explosive?"}
+        ]
+    )
+    response["explosive"] = completion.choices[0].message.content
+    total_tokens += completion["usage"].total_tokens
+
+    print(total_tokens)
     print(response)
     return response
 
