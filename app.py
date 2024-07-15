4b04f9998a1fcb73b8302e9bb4f7dafc97d16b1b
kiwijuice56 <alfaroeric127@gmail.com>
2024-07-15 11:35:00 -0500

    Added helper functions and input validation

--- app.py
+++ app.py
@@ -1,15 +1,64 @@
 import os
+import re
 from dotenv import load_dotenv
-
 from flask import Flask, request
-
 from openai import OpenAI
 
 load_dotenv()
 
 app = Flask(__name__)
-
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+
+possible_states = ['liquid', 'gas', 'rigid_solid', 'powdery_solid', 'metal', 'laser', 'electricity', 'life', 'bomb']
+
+
+def clean_color(color):
+    # Longest valid answer is #FFFFFF
+    color = color[:7]
+
+    # Remove erroneous symbols that sometimes appear
+    color = '#' + ''.join(c for c in color if c.isalnum())
+
+    # Valid HEX format
+    if re.search(r'^#(?:[0-9a-fA-F]{3}){1,2}$', color):
+        return color
+    else:
+        return "#FFFFFF"
+
+
+def clean_state(state):
+    # Remove quotes and other punctuation
+    state = ''.join(c for c in state if c.isalpha() or c == '_')
+
+    # Strangely, ChatGPT sometimes capitalizes answers
+    state = state.lower()
+
+    if state in possible_states:
+        return state
+    else:
+        return 'rigid_solid'
+
+
+def clean_number(number, default):
+    number = ''.join(c for c in number if c.isnumeric())
+
+    try:
+        return float(number)
+    except:
+        return default
+
+
+def prompt(system_message, user_message):
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": system_message},
+            {"role": "user", "content": user_message}
+        ]
+    )
+    return completion.choices[0].message.content, completion.usage.total_tokens
+
 
 @app.route('/')
 def get_custom_element():
@@ -17,58 +66,42 @@ def get_custom_element():
     if len(element_name) == 0:
         return {}
 
-    response = {}
+    response = {"element_name": element_name}
     total_tokens = 0
 
-    completion = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "Answer in HEX format exactly"},
-            {"role": "user", "content": f"Color of '{element_name}'"},
-        ]
-    )
-    response["color_0"] = completion.choices[0].message.content
-    total_tokens += completion.usage.total_tokens
+    # General questions for all elements
 
-    completion = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "Answer in HEX format exactly"},
-            {"role": "user", "content": f"Color of '{element_name}'"},
-        ]
-    )
-    response["color_1"] = completion.choices[0].message.content
-    total_tokens += completion.usage.total_tokens
+    answer, prompt_tokens = prompt("Answer in HEX format exactly", f"Color of '{element_name}'")
+    response["color_0"] = clean_color(answer)
+    total_tokens += prompt_tokens
 
-    completion = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "Answer only an exact entry from the list: [liquid, gas, rigid_solid, powdery_solid, life]"},
-            {"role": "user", "content": f"Best description of '{element_name}'"}
-        ]
-    )
-    response["state"] = completion.choices[0].message.content
-    total_tokens += completion.usage.total_tokens
+    answer, prompt_tokens = prompt("Answer in HEX format exactly", f"Color of '{element_name}'")
+    response["color_1"] = clean_color(answer)
+    total_tokens += prompt_tokens
 
-    completion = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "Answer either Y or N exactly"},
-            {"role": "user", "content": f"Is '{element_name}' flammable?"}
-        ]
-    )
-    response["flammable"] = completion.choices[0].message.content
-    total_tokens += completion.usage.total_tokens
+    answer, prompt_tokens = prompt("Answer only an exact entry from the list: " + str(possible_states),
+                                   f"Best description of '{element_name}'")
+    response["state"] = clean_state(answer)
+    total_tokens += prompt_tokens
 
-    completion = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "Answer either Y or N exactly"},
-            {"role": "user", "content": f"Is '{element_name}' explosive?"}
-        ]
-    )
-    response["explosive"] = completion.choices[0].message.content
-    total_tokens += completion.usage.total_tokens
+    answer, prompt_tokens = prompt("Guess randomly if unknown. Use no words and always respond in one Kelvin (K) number exactly",
+                                   f"Temperature of {element_name}")
+    response["temperature"] = clean_number(answer, 298.0)
+    total_tokens += prompt_tokens
+
+    # State-specific questions
+    match response["state"]:
+        case "fluid":
+            # We need to ask for no units, otherwise all numbers will be parsed with an extra '3' at the end
+            answer, prompt_tokens = prompt("Guess randomly if unknown. Use no words and always respond in one kg * m^-3 number exactly. Do not include units.",
+                                           f"Density of {element_name}")
+            response["density"] = max(0, min(10000, clean_number(answer, 1000.0)))
+            total_tokens += prompt_tokens
+
+            answer, prompt_tokens = prompt("Guess randomly if unknown. Use no words and always respond in one number on a scale from 0.00 to 1.00 exactly, where 0.00 is oil and 1.00 is honey. ",
+                                           f"Viscosity of {element_name}")
+            response["viscosity"] = max(0.0, min(clean_number(answer, 0.5)))
+            total_tokens += prompt_tokens
 
     print(total_tokens)
     print(response)
