0d2f2a86113c9e7a63e7d649a3df7fd7633b1139
kiwijuice56 <alfaroeric127@gmail.com>
2024-07-14 21:56:35 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import flask, request
+from flask_cors import CORS
+
+from openai import OpenAI
+
+app = Flask(__name__)
+CORS(app)
+
+client = OpenAI()
+
+@app.route('/')
+def get_custom_element():
+    element_name = request.args.get("element_name")[:12]
+    if len(element_name) == 0:
+        return {}
+
+
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "Answer in hex only"},
+            {"role": "user", "content": f"Color of: {element_name}"}
+        ]
+    )
+
+    return {"color": completion.choices[0].message}
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
